c5518a82dd7e9a69cd5382101378b3a9bef8a1e1
TuralStep <turalozel@gmail.com>
2022-11-16 12:32:03 +0400

    Add project files.

---
+++ HW_1/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace HW_1;
+
+public interface Button
+{
+    void Render();
+    void OnClick();
+}
+
+public class WindowsButton : Button
+{
+    public void OnClick() =>
+        Console.WriteLine("WindowsButton clicked...");
+
+    public void Render() =>
+        Console.WriteLine("WindowsButton rendered...");
+}
+public class HTMLButton : Button
+{
+    public void OnClick() =>
+        Console.WriteLine("HTMLButton clicked...");
+
+    public void Render() =>
+        Console.WriteLine("HTMLButton rendered...");
+}
+
+
+
+public abstract class Dialog
+{
+    public void Render()
+    {
+        Button okButton = CreateButton();
+        okButton.OnClick();
+        okButton.Render();
+    }
+
+    public abstract Button CreateButton();
+}
+
+public class WindowsDialog : Dialog
+{
+    public override Button CreateButton() =>
+        new WindowsButton();
+}
+public class WebDialog : Dialog
+{
+    public override Button CreateButton() =>
+        new HTMLButton();
+}
---
+++ HW_2/Program.cs
@@ -0,0 +1,108 @@
+﻿namespace HW_2;
+
+public interface Chair
+{
+    bool HasLegs();
+    void SitOn();
+}
+
+public class VictorianChair : Chair
+{
+    public bool HasLegs() =>
+        true;
+
+    public void SitOn() =>
+        Console.WriteLine("Sit on victorian chair...");
+}
+public class ModernChair : Chair
+{
+    public bool HasLegs() =>
+        false;
+
+    public void SitOn() =>
+        Console.WriteLine("Sit on modern chair...");
+}
+
+
+
+public interface CoffeeTable
+{
+    bool HasLegs();
+    void SitOn();
+}
+
+public class VictorianCoffeeTable : CoffeeTable
+{
+    public bool HasLegs() =>
+        true;
+
+    public void SitOn() =>
+        Console.WriteLine("You can't sit on victorian coffee table...");
+}
+public class ModernCoffeeTable : CoffeeTable
+{
+    public bool HasLegs() =>
+        true;
+
+    public void SitOn() =>
+        Console.WriteLine("You can't sit on modern coffee table...");
+}
+
+
+
+public interface Sofa
+{
+    bool HasLegs();
+    void SitOn();
+}
+
+public class VictorianSofa : Sofa
+{
+    public bool HasLegs() =>
+        true;
+
+    public void SitOn() =>
+        Console.WriteLine("Sit on victorian sofa...");
+}
+public class ModernSofa : Sofa
+{
+    public bool HasLegs() =>
+        false;
+
+    public void SitOn() =>
+        Console.WriteLine("Sit on modern sofa...");
+}
+
+
+
+
+
+public interface FurnitureFactory
+{
+    Chair CreateChair();
+    CoffeeTable CreateCoffeeTable();
+    Sofa CreateSofa();
+}
+
+public class VictorianFurnitureFactory : FurnitureFactory
+{
+    public Chair CreateChair() =>
+        new VictorianChair();
+
+    public CoffeeTable CreateCoffeeTable() =>
+        new VictorianCoffeeTable();
+
+    public Sofa CreateSofa() =>
+        new VictorianSofa();
+}
+public class ModernFurnitureFactory : FurnitureFactory
+{
+    public Chair CreateChair() =>
+        new ModernChair();
+
+    public CoffeeTable CreateCoffeeTable() =>
+        new ModernCoffeeTable();
+
+    public Sofa CreateSofa() =>
+        new ModernSofa();
+}
